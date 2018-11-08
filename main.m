b0f2c6eef6aa9d35eac4af5150ef323f9d24e6bf
trainlock <trainlock@gmail.com>
2018-11-08 11:18:42 +0100

    Created main and the base for function

---
+++ main.m
@@ -0,0 +1,5 @@
+% Read image
+filename = "./Images_Training/im1s.jpg";
+im = imread(filename);
+
+strout = tnm034(im);
---
+++ tnm034.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%% 
+function strout = tnm034(im) 
+% 
+% Im: Inputimage of captured sheet music. Im should be in
+% double format, normalized to the interval [0,1] 
+% strout: The resulting character string of the detected 
+% notes. The string must follow a pre-defined format. 
+% 
+% Your program code. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%
