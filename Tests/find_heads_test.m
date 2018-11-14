07baaf01c081ee54252cc2cc8b373097583fc122
Emma Broman <emmbr740@student.liu.se>
2018-11-14 14:26:30 +0100

    Find note heads and their centroids

---
+++ Tests/find_heads_test.m
@@ -0,0 +1,32 @@
+% Read image
+clear;
+filename = '././Images_Training/im1s.jpg';
+im = imread(filename);
+imshow(im)
+
+[im_BW, im] = invertAndRotate(im);
+
+% Test 1: Opening with ciruclar (disc) structuring element
+SE = strel('disk',4); % Structuring element
+J = imopen(im_BW,SE);
+
+L = bwlabel(J);
+s = regionprops(L,'centroid');
+centroids = cat(1, s.Centroid);
+imshow(im_BW)
+hold on
+plot(centroids(:,1),centroids(:,2), 'b*')
+hold off
+
+% figure
+% subplot(2,1,1)
+% imshow(J)
+% subplot(2,1,2)
+% imshow(im_BW)
+
+
+
+
+
+
+
