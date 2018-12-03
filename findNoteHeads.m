c851f825eb48503c12f8cf7d69f1fd07067b3041
Emma Broman <emmbr740@student.liu.se>
2018-12-03 11:55:35 +0100

    Update findNoteHeads.m so that is uses the line distance for morphological operations

--- findNoteHeads.m
+++ findNoteHeads.m
@@ -1,16 +1,20 @@
-function [heads] = findNoteHeads(BW)
+function [heads] = findNoteHeads(BW, d)
 %FINDNOTEHEADS find the positions of the note heads 
 % Input:
 %   BW - binary image to find circular note heads in
+%   d -  Distance between two lines, hiven as interval d = [dMin, dMax]
 % Output:
 %   heads - the resulting positions of the note heads. One row per head. 
 
+% Average distance between lines (avg height of note head)
+d_avg = (d(2)+d(1))/2; 
+
 % first remove any thick beams using opening
-IM = imopen(BW,strel('rectangle',[4,15])); % OBS! Size should be dependent on image scale. 
+IM = imopen(BW,strel('rectangle',[floor(d_avg/3),floor(2*d_avg)]));
 IM = BW-IM; % Remove the beams.
 
 % Opening with ciruclar (disk) structuring element to separate the heads
-IM = imopen(IM,strel('disk',4)); % OBS! Same here. Size based on image scale
+IM = imopen(IM,strel('disk',floor(d_avg/2))); % OBS! Same here. Size based on image scale
 
 % Find the note head positions (centroids of objects in IM)
 L = bwlabel(IM);
