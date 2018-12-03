748abb4af45f5e91245cff08aa7d57dc17a7d062
Emma Broman <emma95.broman@hotmail.com>
2018-12-03 21:12:13 +0100

    Pitch detection more accurate

--- Tests/pitch_test.m
+++ Tests/pitch_test.m
@@ -1,5 +1,5 @@
 format compact
-filename = './Images_Training/im3s.jpg';
+filename = './Images_Training/im1s.jpg';
 im = imread(filename);
 im = rgb2gray(im);
 im = imresize(im,1.8); % scale image (bicubic interpolation by default)
@@ -57,31 +57,21 @@ end
 %% For each sub-image: identify note head and its pitch
 
 centerLine = lineIndices(3);
-avgLineDist = (d(2)+d(1))/2 + (n(2)-n(1))/2; %(lineIndices(end)-lineIndices(1))/(length(lineIndices)-1);
-%avgLineDist = (lineIndices(end)-lineIndices(1))/(length(lineIndices)-1);
-pitchDist = round(avgLineDist/2);
+%avgLineDist = (d(2)+d(1))/2 + (n(2)-n(1))/2; %(lineIndices(end)-lineIndices(1))/(length(lineIndices)-1);
+avgLineDist = (lineIndices(end)-lineIndices(1))/(length(lineIndices)-1);
+pitchDist = avgLineDist/2;
 
-encoding = ["g1",'a1','b1','c2','d2','e2','f2','g2','a2','b2','c3','d3','e3','f3','g3','a3','b3','c4','d4','e4'];
-%positions = int8(centerLine+9*pitchDist: -pitchDist : centerLine-10*pitchDist);
-positions = uint8(zeros(1, length(encoding)));
-
-% Set positions: Manually to achieve an as high accuracy as possible
-% TODO: CODE THIS!!!
-positions(10) = centerLine;
-positions(11:end) = (centerLine - pitchDist : -pitchDist : centerLine-10*pitchDist) 
-%%
-% OBS! Might want to compute and use line width and sompute distance
-% between lines differently...
+encoding = ['g1'; 'a1'; 'b1'; 'c2'; 'd2'; 'e2'; 'f2'; 'g2'; 'a2'; 'b2'; 'c3'; 'd3'; 'e3'; 'f3'; 'g3'; 'a3'; 'b3'; 'c4'; 'd4'; 'e4'];
+encoding = transpose(encoding);
+positions = round(centerLine+9*pitchDist: -pitchDist : centerLine-10*pitchDist);
 
 % TEST: draw line positions
-% im_test = subIms(:,:,1);
-% RGB = cat(3,im_test,im_test,im_test);
-% RGB(positions, :, 1) = 255;
-% RGB(positions-1, :, 1) = 255;
-% RGB(positions+1, :, 1) = 255;
-% RGB(centerLine, :, 2) = 255;
-% figure
-% imshow(RGB);
+im_test = subIms(:,:,1);
+RGB = cat(3,im_test,im_test,im_test);
+RGB((positions-1:1:positions+1), :, 1) = 255;
+RGB(centerLine, :, 2) = 255;
+figure
+imshow(RGB);
 
 %%
 res = ''; % empty string for result
@@ -95,24 +85,22 @@ for i = 1:size(BW_subIms,3)
         % TEST: Show the centroids
         %RGB = cat(3,subIms(:,:,i),subIms(:,:,i),subIms(:,:,i));
         %RGB(positions(2:2:end), :, 1) = 255;
-        figure
-        %imshow(RGB);
-        imshow(currentIm)
-        hold on
-        plot(heads(:,1),heads(:,2), 'b*')
-        hold off
+%         figure
+%         %imshow(RGB);
+%         imshow(currentIm)
+%         hold on
+%         plot(heads(:,1),heads(:,2), 'b*')
+%         hold off
     end
     
     % For each centroid, encode the pitch
     for j = 1:length(heads)
         c_row = heads(j,2);  % col 2 = y = row
-        [c index] = min(abs(c_row-positions)); % find closest possible pitch pos
-        res = [res, encoding(index)];
+        diff = abs(c_row-positions);
+        [c index] = min(diff); % find closest possible pitch pos
+        res = [res, strcat(encoding(1,index), encoding(2,index))];
     end
     
     res = [res, 'n'];
     
-    % OBS!!! The lines are not fully horizontal, so the patch is encoded
-    % incorrectly several times :( 
-    % TODO: Fix this!!!
 end
