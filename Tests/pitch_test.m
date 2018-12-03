379a004e867625650f3ee1edc6e8bc72c3488e85
Emma Broman <emmbr740@student.liu.se>
2018-12-03 12:11:32 +0100

    Working on updated pitch classification

--- Tests/pitch_test.m
+++ Tests/pitch_test.m
@@ -2,7 +2,7 @@ format compact
 filename = './Images_Training/im3s.jpg';
 im = imread(filename);
 im = rgb2gray(im);
-im = imresize(im,2); % scale image (bicubic interpolation by default)
+im = imresize(im,1.8); % scale image (bicubic interpolation by default)
 
 % Rotate image, find and remove lines and clip image to subimages
 
@@ -13,12 +13,21 @@ im = imresize(im,2); % scale image (bicubic interpolation by default)
 % Make binary and invert (0->1, 1->0)
 [BW, im] = invertAndRotate(im);
 
+% Compute distances n (line width) and d (line distance)
+[d, n] = computeStaffMetrics(BW);
+
 % Find lines and these save row indices
 lineIndices = findLineIndices(BW);
 
 % Create subimages containing one row each
 subIms = createSubImages(im, lineIndices);
 
+% TEST: draw line positions
+% RGB = cat(3,im,im,im);
+% RGB(lineIndices, :, 1) = 255;
+% figure
+% imshow(RGB);
+%%
 % Compute level to use for thresholding
 level = graythresh(subIms); 
 
@@ -33,49 +42,59 @@ for i = 1:size(subIms,3)
     % binarize subimage
     BW_subIms(:,:,i) = im2bw(subIms(:, :, i), level);
     % Remove lines
-    BW_subIms(:,:,i) = removeLines(BW_subIms(:,:,i));
-    figure
-    imshow(BW_subIms(:,:,i))
+    BW_subIms(:,:,i) = removeLines(BW_subIms(:,:,i), d);
+%     figure
+%     imshow(BW_subIms(:,:,i))
 end
 
 % TEST: draw line positions
-im_test = subIms(:,:,1);
-RGB = cat(3,im_test,im_test,im_test);
-RGB(lineIndices, :, 1) = 255;
-figure
-imshow(RGB);
+% im_test = subIms(:,:,1);
+% RGB = cat(3,im_test,im_test,im_test);
+% RGB(lineIndices, :, 1) = 255;
+% figure
+% imshow(RGB);
 
 %% For each sub-image: identify note head and its pitch
 
 centerLine = lineIndices(3);
-avgLineDist = (lineIndices(end)-lineIndices(1))/(length(lineIndices)-1);
-pitchDist = floor(avgLineDist/2);
+avgLineDist = (d(2)+d(1))/2 + (n(2)-n(1))/2; %(lineIndices(end)-lineIndices(1))/(length(lineIndices)-1);
+%avgLineDist = (lineIndices(end)-lineIndices(1))/(length(lineIndices)-1);
+pitchDist = round(avgLineDist/2);
 
 encoding = ["g1",'a1','b1','c2','d2','e2','f2','g2','a2','b2','c3','d3','e3','f3','g3','a3','b3','c4','d4','e4'];
-positions = int8(centerLine+9*pitchDist: -pitchDist : centerLine-10*pitchDist);
+%positions = int8(centerLine+9*pitchDist: -pitchDist : centerLine-10*pitchDist);
+positions = uint8(zeros(1, length(encoding)));
 
+% Set positions: Manually to achieve an as high accuracy as possible
+% TODO: CODE THIS!!!
+positions(10) = centerLine;
+positions(11:end) = (centerLine - pitchDist : -pitchDist : centerLine-10*pitchDist) 
+%%
 % OBS! Might want to compute and use line width and sompute distance
 % between lines differently...
 
 % TEST: draw line positions
-im_test = subIms(:,:,1);
-RGB = cat(3,im_test,im_test,im_test);
-RGB(positions(2:2:end), :, 1) = 255;
-figure
-imshow(RGB);
+% im_test = subIms(:,:,1);
+% RGB = cat(3,im_test,im_test,im_test);
+% RGB(positions, :, 1) = 255;
+% RGB(positions-1, :, 1) = 255;
+% RGB(positions+1, :, 1) = 255;
+% RGB(centerLine, :, 2) = 255;
+% figure
+% imshow(RGB);
 
-
+%%
 res = ''; % empty string for result
 for i = 1:size(BW_subIms,3)
     
     currentIm = BW_subIms(:,:,i);
     
-    heads = findNoteHeads(currentIm);
+    heads = findNoteHeads(currentIm, d);
     
     if(length(heads) > 1)
         % TEST: Show the centroids
-        RGB = cat(3,subIms(:,:,i),subIms(:,:,i),subIms(:,:,i));
-        RGB(positions(2:2:end), :, 1) = 255;
+        %RGB = cat(3,subIms(:,:,i),subIms(:,:,i),subIms(:,:,i));
+        %RGB(positions(2:2:end), :, 1) = 255;
         figure
         %imshow(RGB);
         imshow(currentIm)
