5a9b8f7b394e3dd35cc15e35054ced4b5eac9fe8
Emma Broman <emmbr740@student.liu.se>
2018-12-05 15:44:21 +0100

    Remove additional cleaning from removeLines.m. Should be done in main instead

--- removeLines.m
+++ removeLines.m
@@ -13,12 +13,6 @@ d_avg = (d(2)+d(1))/2;
 % Add to binary images into one complete binary image withouth any lines
 BWnl = imopen(BW, strel('line', floor(d_avg/3), 90));
 
-% Try to fix accidentally broken objects a bit. 
-BWnl = bwmorph(BWnl, 'close', 2);
-
-% Remove all objects that has a smaller area 
-BWnl = bwareaopen(BWnl, floor(3.14*(d_avg/2)*(d_avg/2)));
-
 imWithoutLines = BWnl;
 
 end
